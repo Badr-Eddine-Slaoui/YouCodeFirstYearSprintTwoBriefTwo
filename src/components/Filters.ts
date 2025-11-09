66bc948a4b1c3f3778e366c8452beb0ae2db76e8
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-11-09 15:08:41 +0100

    Add Search Functionality Closed #15 | Add Filters (Genre, Platform, Rating) Closed #16

--- src/components/Filters.ts
+++ src/components/Filters.ts
@@ -1,20 +1,21 @@
+import type { makeState } from "../core";
 import { Select } from "./Select";
 
-const url = "https://debuggers-games-api.duckdns.org";
+const api = "https://debuggers-games-api.duckdns.org";
 
-export const Filters = async (): Promise<HTMLElement> => {
+export const Filters = async (url: ReturnType<typeof makeState<string>>): Promise<HTMLElement> => {
+  const [{ genres }, { platforms }] = await Promise.all([
+    fetch(`${api}/api/genres`).then((res) => res.json()),
+    fetch(`${api}/api/platforms`).then((res) => res.json()),
+  ]);
 
-    const [{ genres }, { platforms }] = await Promise.all([
-        fetch(`${url}/api/genres`).then((res) => res.json()),
-        fetch(`${url}/api/platforms`).then((res) => res.json())
-    ]);
+  const container = document.createElement("div") as HTMLDivElement;
+  container.className =
+    "flex h-[4vh] gap-4 justify-start items-center px-4 text-[2.5rem]";
 
-    const container = document.createElement("div") as HTMLDivElement;
-    container.className = "flex h-[4vh] gap-4 justify-start items-center px-4 text-[2.5rem]";
+  container.append(Select(url, ["Genre", ...genres]));
+  container.append(Select(url, ["Platform", ...platforms]));
+  container.append(Select(url, ["Rating", "1", "2", "3", "4", "5"]));
 
-    container.append(Select(["Genre", ...genres]));
-    container.append(Select(["Platform", ...platforms]));
-    container.append(Select(["Rating", "1", "2", "3", "4", "5"]));
-
-    return container;
-}
+  return container;
+};
